103f9dde4bed9e388b5d6e00e83df502babb2722
hoangha922 <hoangha922@gmai.com>
2024-12-06 22:59:37 +0700

    Initial commit

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestDemo {
+
+    static WebDriver driver = null;
+
+    public static void main(String[] args) {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://serenity-bdd.github.io/");
+        driver.quit();
+    }
+}
